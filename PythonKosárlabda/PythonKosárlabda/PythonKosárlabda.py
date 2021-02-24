68c33a2a6baa521506ee39f7a79070e2334e4314
lamartin0410 <lamartin0410@gmail.com>
2021-02-24 13:04:58 +0100

    Végleges

--- PythonKosárlabda/PythonKosárlabda/PythonKosárlabda.py
+++ PythonKosárlabda/PythonKosárlabda/PythonKosárlabda.py
@@ -1,12 +1,10 @@
 import filebe
 import harmas
-<<<<<<< HEAD
 import negyes
 import otos
-import feladat7
 import feladat6
+import feladat7
 
->>>>>>> a8fa0446c7f836ce85ddc0529f7d9c4f865bcbaf
 fileName = "eredmenyek.csv"
 eredmenyek = []
 
@@ -14,11 +12,5 @@ filebe.filebeolvasas(eredmenyek, fileName)
 harmas.hany(eredmenyek)
 negyes.volte(eredmenyek)
 otos.nev(eredmenyek)
-feladat7.feladat7(eredmenyek)
-<<<<<<< HEAD
-negyes.volte(eredmenyek)
-=======
-feladat7.feladat7(eredmenyek)
->>>>>>> a8fa0446c7f836ce85ddc0529f7d9c4f865bcbaf
 feladat6.feladat6(eredmenyek)
 feladat7.feladat7(eredmenyek)
