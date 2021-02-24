dcc89391771c54b792c686ff413695b9fb62611a
lamartin0410 <lamartin0410@gmail.com>
2021-02-24 12:55:31 +0100

    feladat6

--- PythonKosárlabda/PythonKosárlabda/PythonKosárlabda.py
+++ PythonKosárlabda/PythonKosárlabda/PythonKosárlabda.py
@@ -1,10 +1,12 @@
 import filebe
 import harmas
 import feladat7
+import feladat6
 
 fileName = "eredmenyek.csv"
 eredmenyek = []
 
 filebe.filebeolvasas(eredmenyek, fileName)
 harmas.hany(eredmenyek)
+feladat6.feladat6(eredmenyek)
 feladat7.feladat7(eredmenyek)
